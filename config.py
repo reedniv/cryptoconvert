46457dadc8d87e3d2741fe44895f4cf418a5b4c4
reedniv <26852596+reedniv@users.noreply.github.com>
2020-01-01 18:56:17 +0700

    Create config.py

---
+++ config.py
@@ -0,0 +1,3 @@
+__TOKEN__ = '1001386650:AAErsu8QUrYWLol0RST6pNhawdnHXF-wduQ'
+__LOCALE_BILLION__ = 'bil.' #In Germany we call a Billion a "Milliarde" instead of "Billionen" which would be a Trillion in english
+__ADMINS__ = [369613251,]
